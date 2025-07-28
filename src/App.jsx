0b9b97b32f68d6c10597d53973e83b66f6a963a2
SURYA PRAKASH <isurya2005@gmail.com>
2025-07-28 21:58:20 +0530

    frontend project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>CI/CD Cloud Devops - Practice</h1>
+      <p>This is a react app hosted using github pages</p>
+    </div>
+  )
+}
+
+export default App
